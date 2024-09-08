a7ab5c321392b648a8ceb6104e94fd2b2dab2585
RyanWang1119 <enrui_wang@ucsb.edu>
2024-09-08 17:09:47 -0400

    add sth

--- hw_1.R
+++ hw_1.R
@@ -1,8 +1,10 @@
+library(ggplot2)
+
 set.seed(98)
 x_i <- runif(1000, min = 0, max = 1)  # simulate from the Unif[0,1]
-y_i <- sqrt(-5*log(1-x_i))  # apply the inverse function
+y_i <- sqrt(-5*log(1-x_i))  # apply the inverse function to get Weibull distributed data
 
-plot(y_i)
+plot(y_i, ylab = "Values")  # simulated data
 eq_1 <- quantile(y_i, seq(0.01, 0.99, length = 100))  # quantile of the simulated data
 
 simulated_data <- data.frame(Quantile = seq(0.01, 0.99, length = 100), Value = eq_1)
