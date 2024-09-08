4d494d74cdf8a4e5b13524be7f2acd7cd7e664d3
RyanWang1119 <enrui_wang@ucsb.edu>
2024-09-08 17:06:53 -0400

    hw_1

---
+++ hw_1.R
@@ -0,0 +1,17 @@
+set.seed(98)
+x_i <- runif(1000, min = 0, max = 1)  # simulate from the Unif[0,1]
+y_i <- sqrt(-5*log(1-x_i))  # apply the inverse function
+
+plot(y_i)
+eq_1 <- quantile(y_i, seq(0.01, 0.99, length = 100))  # quantile of the simulated data
+
+simulated_data <- data.frame(Quantile = seq(0.01, 0.99, length = 100), Value = eq_1)
+
+theoretical_data <- data.frame(Quantile = seq(0.01, 0.99, length = 100),
+                               Value = qweibull(p = seq(0.01, 0.99, length = 100), shape = 2, scale = sqrt(5)))
+
+ggplot() +
+  geom_line(data = simulated_data, aes(x = Quantile, y = Value), color = "blue", linetype = "dashed", size =1) +
+  geom_line(data = theoretical_data, aes(x = Quantile, y = Value), color = "red", linetype = "solid", size =1) +
+  labs(x = "Quantiles", y = "Values")
+
